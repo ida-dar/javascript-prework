6c294e19a0d6086053a1945225319de27118e261
Ida Darmochwał <ida.mdart@gmail.com>
2021-04-01 14:20:17 +0200

    Add variables

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove ="kamień";
+printMessage("Zagrałem" + computerMove + "Jesli twój ruch to papier, to wygrywasz!");
+
+let playerMove = "papier";
+printMessage("Zagrałem" + playerMove);
