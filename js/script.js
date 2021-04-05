af1a1bb0dd38c70748219441a015798e9b1e7582
Ida Darmochwał <ida.mdart@gmail.com>
2021-04-06 00:21:36 +0200

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-let computerMove ="kamień";
-let playerMove = "papier";
-printMessage("Zagrałem " + computerMove + "! Jesli twój ruch to papier, to wygrywasz! " + "<br>" + "Zagrałem " + playerMove + " - wygrałem!");
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log("Wylosowana liczba to: " + randomNumber);
+
+let computerMove = "nieznany ruch";
+
+if(randomNumber == 1) {
+    computerMove = "kamień";
+} else if (randomNumber == 2) {
+    computerMove = "papier";
+} else {
+    computerMove = "nożyce";
+}
+
+printMessage("Mój ruch to: " + computerMove);
+
