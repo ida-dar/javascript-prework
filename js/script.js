4789a9a1737544f77a62362ddbcbb4bff3ff1004
Ida Darmochwał <ida.mdart@gmail.com>
2021-04-06 13:48:55 +0200

    Edit playerMove & add game result

--- js/script.js
+++ js/script.js
@@ -16,16 +16,36 @@ printMessage("Mój ruch to: " + computerMove);
 
 let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log("Gracz wpisał: " + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = "nieznany ruch";
 
-if(playerInput == "1"){
-  playerMove = 'kamień';
-} else if (playerInput = "2") {
+if(playerInput == "1") {
+  playerMove = "kamień";
+} else if (playerInput == "2") {
     playerMove = "papier";
-} else {
+} else if (playerInput == "3") {
     playerMove = "nożyce";
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage("Twój ruch to: " + playerMove);
+
+
+if(computerMove == "kamień" && playerMove == "papier") {
+    printMessage("Ty wygrywasz!");
+
+} else if(computerMove == "papier" && playerMove == "nożyce") {
+    printMessage("Ty wygrywasz!");
+
+} else if(computerMove == "nożyce" && playerMove == "papier") {
+    printMessage("Ty wygrywasz!");
+
+} else if(computerMove == playerMove) {
+    printMessage("Remis!");
+
+} else if(playerMove == "nieznany ruch") {
+    printMessage("Wpisano niepoprawną wartość");
+
+} else {
+    printMessage("Przegrałeś");
+}
